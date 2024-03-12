4e849072a5b6c742d182ac62847b73fc101dd91f
mhaemnn <muhaemin1998@gmail.com>
2024-03-12 21:32:44 +0700

    create file aritmatika

---
+++ #7-aritmaika.py
@@ -0,0 +1,20 @@
+a = 10
+b = 5
+selisih = a - b
+jumlah  = a + b
+kali    = a * b
+bagi    = a / b
+
+#cetak output
+print("hasil selisih dari a dan b: ", selisih)
+print("hasil jumlah dari a dan b: ", jumlah)
+print("hasil kali dari a dan b: ", kali)
+print("hasil bagi dari a dan b: ", bagi)
+
+
+# program untuk menentukan apakah varible bilangan genep atau ganjil
+angka = 10
+if(angka % 2 == 0):
+  print("angka bilangan genep")
+else:
+  print("angka bilangan ganjil")
