d39eb8c926e282582abf837a830dafd531e88229
mhaemnn <muhaemin1998@gmail.com>
2024-03-12 16:15:23 +0700

    if statement

---
+++ if-statement.py
@@ -0,0 +1,4 @@
+i = 10 # inisialisasi varibale i yang memiliki nilia  10
+if(i == 10): #pengecekan niali i apakah sama dengan 10
+  print("ini adalah angka 10") # jika TRUE maka akan mencetak kalimat ini 
+
