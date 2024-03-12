f2658d6b4e8ac70f4c490e7f64f1fe961f7f55e4
mhaemnn <muhaemin1998@gmail.com>
2024-03-12 16:24:59 +0700

    create file if-elif-else

---
+++ #4-if-else.py
@@ -0,0 +1,5 @@
+i = 10
+if(i == 10):
+  print("ini adalah angka 10")
+else:
+  print("bukan angka 10")
---
+++ #5-if-elif-else.py
@@ -0,0 +1,7 @@
+i = 5
+if(i == 5 ):
+	print("ini adalah angka 5")
+elif(i > 5):
+	print("lebih besar dari 5")
+else:
+	print("lebih kecil dari 5")
