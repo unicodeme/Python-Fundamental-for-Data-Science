b36b66265e997a1f990c20750b63119d50f1952b
mhaemnn <muhaemin1998@gmail.com>
2024-03-12 15:34:13 +0700

    add file printing-data-type

---
+++ printing-data-type.py
@@ -0,0 +1,52 @@
+#tipe data boolean 
+print(True)
+
+#tipe data string
+print("Ayo belajar Python")
+print("Belajar Python Sangat Mudah di DQLAB")
+
+#tipe data integer
+print(20)
+
+#tipe data float
+print(23.1)
+
+#tipe data list
+print([1,2,3,4,5])
+print(["satu","dua","tiga"])
+
+#tipe data tuple
+print((1,2,3,4,5))
+print(("satu","dua","tiga"))
+
+#tipe data dictionary 
+print({"nama": "muhaemin", "umur": 26})
+
+
+#tipe data dictionary dimasukan ke dalam varible biodata 
+biodata = {"nama": "sadam", "umur": 24} #proses inisialisasi varibale biodata 
+print(biodata) #proses mencetak variable biodata yang berisi tipe data dictionary
+
+
+# TUGAS PRAKTEK
+var_string = "Belajar Python DQLab"
+var_int = 10
+var_float = 3.14
+var_list = [1,2,3,4,5]
+var_tuple = ("satu","dua","tiga")
+var_dict = {"nama": "sanukuli", "umur":35}
+
+print(var_string)
+print(var_int)
+print(var_float)
+print(var_list)
+print(var_tuple)
+print(var_dict)
+
+print(type(var_string))
+print(type(var_int))
+print(type(var_float))
+print(type(var_list))
+print(type(var_tuple))
+print(type(var_dict))
+
