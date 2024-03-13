918d370c20f73736053ec74ebc2eb153766f8f5c
mhaemnn <muhaemin1998@gmail.com>
2024-03-13 10:15:11 +0700

    create file looping-for

---
+++ #9-looping-for.py
@@ -0,0 +1,9 @@
+# for (1)
+for i in range(1,6):  #perulangan for sebagai inisialisasi dari angka 1 hingga angka lebih kecil daripada 6
+  print("Ini adalah perualangan ke - ", i) #perintah jika looping akan tetap berjalan
+
+# for (2)
+# with access element 
+count = [1,2,3,4,5]
+for number in count: 
+  print("Ini adalah element count: ", number)
